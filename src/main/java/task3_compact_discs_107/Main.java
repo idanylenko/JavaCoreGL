efcf4b63d160f8bdb536600647693ff5deedd431
idanylenko <idanylenko@talend.com>
2019-05-10 16:18:31 +0300

    fixed task 3 (compact discs)

--- src/main/java/task3_compact_discs_107/Main.java
+++ src/main/java/task3_compact_discs_107/Main.java
@@ -10,16 +10,16 @@ public class Main {
 
             if (n / 100 > 0) {
                 packs_100 = n / 100;
-            } else if ((n - packs_100 * 100) > 20) {
+            }
+            if ((n - packs_100 * 100) > 20) {
                 packs_20 = (n - packs_100 * 100) / 20;
-            } else if ((n - packs_100 * 100 - packs_20 * 20) > 0) {
+            }
+            if ((n - packs_100 * 100 - packs_20 * 20) > 0) {
                 one_disk = n - packs_100 * 100 - packs_20 * 20;
             }
             sum = packs_100 * 100 + packs_20 * 30 + one_disk * 2;
+
             System.out.println(sum);
-        System.out.println("Condition 1: " + (n - packs_100 * 100));
-        System.out.println("Packs of 20: " + ((n - packs_100 * 100) / 20));
-        System.out.println("100 packs: " + packs_100 + " 20 packs: " + packs_20 + " one disks: " + one_disk);
 
     }
 }
