ec6812619a108334b024a57aab037cc8cf715e17
idanylenko <idanylenko@talend.com>
2019-05-10 16:56:02 +0300

    Added task 15

--- src/main/java/task15_rectangular_or_not_915/Main.java
+++ src/main/java/task15_rectangular_or_not_915/Main.java
@@ -1,4 +1,4 @@
-package task14_type_of_triangle_905;
+package task15_rectangular_or_not_915;
 
 import java.util.Arrays;
 import java.util.Scanner;
@@ -14,15 +14,13 @@ public class Main {
         Arrays.sort(sides);
 
         if (sides[2] < sides[1] + sides[0]) {
-        if (a == b && b == c) {
-            System.out.println(1);
-        } else if (a == b || a == c || b == c) {
-            System.out.println(2);
+
+            if (Math.pow(sides[2], 2) == (Math.pow(sides[0], 2) + Math.pow(sides[1], 2))) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
         } else {
-            System.out.println(3);
-        }
-        }
-        else {
             System.out.println("Not possible to create a triangle with given sides length");
         }
 
