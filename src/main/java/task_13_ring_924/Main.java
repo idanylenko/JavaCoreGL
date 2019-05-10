cc3c8234113f49bcc2b3f94957dfcbf0ae7add3f
idanylenko <idanylenko@talend.com>
2019-05-10 16:33:58 +0300

    Added task 13

---
+++ src/main/java/task_13_ring_924/Main.java
@@ -0,0 +1,13 @@
+package task_13_ring_924;
+
+public class Main {
+    public static void main(String[] args) {
+        double ring_area = 50.2655;
+        int r_big = 5;
+        double r_small;
+
+        r_small = Math.sqrt((Math.PI * r_big * r_big - ring_area) / Math.PI);
+        System.out.printf ("%.0f", r_small);
+
+    }
+}
