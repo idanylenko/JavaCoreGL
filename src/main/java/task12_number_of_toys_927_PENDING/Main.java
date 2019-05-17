fd776b79a44131acbae97f3ea7dc3a61f3ae4e75
idanylenko <idanylenko@talend.com>
2019-05-17 16:56:57 +0300

    added initial code for task 12

--- src/main/java/task12_number_of_toys_927_PENDING/Main.java
+++ src/main/java/task12_number_of_toys_927_PENDING/Main.java
@@ -1,4 +1,32 @@
 package task12_number_of_toys_927_PENDING;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        int n;
+        int countToysLess50 = 0;
+
+        Scanner in = new Scanner(System.in);
+        n = in.nextInt();
+        int[]quantity = new int[n];
+//        double[]price = new double[n];
+
+        while (in.hasNext()) {
+            for (int i = 0; i < n*2; i++) {
+                quantity[i] = in.nextInt();
+                if(in.nextDouble()<50){
+                    countToysLess50 += quantity[i];
+                }
+            }
+        }
+
+
+
+        System.out.println(n);
+
+
+
+        System.out.println(countToysLess50);
+    }
 }
